0cdb05adec8c3b80f05be857003a291199490b40
Belov Dmitrii <belovd90@mail.ru>
2023-12-24 18:46:15 +0300

    Время разбивать камни

--- src/main.js
+++ src/main.js
@@ -1,15 +1,17 @@
 import { RenderPosition, render } from './render';
-
-import NewTripInfoView from './View/Trip-info/trip-info';
 import NewTripFilterView from './View/Trip-filter/trip-filter';
-import TripSortView from './View/Trip-sort/trip-sort';
-import RefactorFormList from './View/Refactor-form/refactor-form-list';
+import NewTripInfoView from './View/Trip-info/trip-info';
+import EventPresenter from './Presenter/event-presenter';
 
-const siteTripMainElement = document.querySelector('.trip-main');
 const siteTripControlsFilterElement = document.querySelector('.trip-controls__filters');
+const siteTripMainElement = document.querySelector('.trip-main');
 const siteTripEventsElement = document.querySelector('.trip-events');
+
+const eventPresenter = new EventPresenter({
+  container: siteTripEventsElement
+});
 
 render(new NewTripInfoView(), siteTripMainElement, RenderPosition.AFTERBEGIN);
 render(new NewTripFilterView(), siteTripControlsFilterElement);
-render(new TripSortView(), siteTripEventsElement);
-render(new RefactorFormList(), siteTripEventsElement);
+
+eventPresenter.init();
